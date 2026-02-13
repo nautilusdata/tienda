3032f96ea08bea3ecefad5a6a10b0dcf4783110b
nautilusdata <rayarza@nautilus.cl>
2026-02-12 23:14:40 -0300

    archivos esenciales

---
+++ home/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('', home_inicio, name="home_inicio"),
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
-# Create your views here.
+def home_inicio(request):
+    return render(request, 'home/home.html', {})
