11f15ece0f96c68f603346a44cfa9ff05c8e8635
nautilusdata <rayarza@nautilus.cl>
2026-02-14 03:58:25 -0300

    views.py para proximo ciclo FOR y mostrar  la base de datos replicada en la caja del frontend.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-def home_inicio(request):
-    return render(request, 'home/home.html', {})
+from home.models import *
+
+def home_inicio(request): 
+	#SELECT *FROM productos where estado id=1 order by id desc limit 8    (recordar: estado id1=Activos)
+    datos=Producto.objects.filter(estado_id=1).order_by('-id').all()[:8]
+    return render(request, 'home/home.html', {'datos':datos})
