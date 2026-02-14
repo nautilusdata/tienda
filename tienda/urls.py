d71176d67a834884d5b439857b41de1b3fec1c03
nautilusdata <rayarza@nautilus.cl>
2026-02-14 01:19:12 -0300

    urls.py principal se añadió ruta para el admin backend

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('core/backend/', admin.site.urls),
     path('', include('home.urls'), name="home_urls"),
     #path('acceso/', include('acceso.urls'), name="acceso_urls"),
     #path('productos/', include('productos.urls'), name="productos_urls"),
