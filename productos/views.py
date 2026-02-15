11f3da52f0365e62f08b1fe9127af1519e7fe2d9
nautilusdata <rayarza@nautilus.cl>
2026-02-15 03:00:26 -0300

    ESTABA OK me volé, el desplegable siempre funcionó bien. Hemos vuelto a los originales.

--- productos/views.py
+++ productos/views.py
@@ -3,17 +3,10 @@ from django.shortcuts import render
 from home.models import *
 
 def productos_detalle(request, id, slug):
-    try:
-        datos = Producto.objects.filter(pk=id).filter(slug=slug).filter(estado_id=1).get()
+    try: #para chequear que lo que vienepor la URL es válido
+        datos=Producto.objects.filter(pk=id).filter(slug=slug).filter(estado_id=1).get()
     except Producto.DoesNotExist:
         raise Http404
-    
-    relacionados = Producto.objects.filter(estado_id=1, producto_categoria=datos.producto_categoria_id).filter(estado_id=1).all()
-    fotos = ProductoFotos.objects.filter(producto_id=id).all()
-    
-    return render(request, 'productos/detalle.html', {
-        'datos': datos,
-        'relacionados': relacionados,
-        'fotos': fotos,
-        'stock_range': range(1, datos.stock + 1)  # <-- esto
-    })
+    relacionados=Producto.objects.filter(estado_id=1, producto_categoria=datos.producto_categoria_id).filter(estado_id=1).all()
+    fotos=ProductoFotos.objects.filter(producto_id=id).all()
+    return render(request, 'productos/detalle.html', {'datos':datos, 'relacionados': relacionados, 'fotos':fotos})
