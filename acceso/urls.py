a1c2a567c18381008d728761b955b5783b59b403
nautilusdata <rayarza@nautilus.cl>
2026-02-15 15:49:39 -0300

    acceso/urls.py patterns estaba mal escrito, y registri en vez de registro. Además Gemini suguiere slashes al final para que no de error cuando un usuario no ponga la barra en la url

--- acceso/urls.py
+++ acceso/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import *                    #llamado a la vistas con las que vamos a trabajar
 
-urlspatterns = [
-path('login', acceso_login, name='acceso_login'),  #permite crear form para que user se pueda loguear
-path('registri', acceso_registro, name='acceso_registro'),
-path('salir', acceso_salir, name='acceso_salir'),
+urlpatterns = [
+path('login/', acceso_login, name='acceso_login'),  #permite crear form para que user se pueda loguear
+path('registro/', acceso_registro, name='acceso_registro'),
+path('salir/', acceso_salir, name='acceso_salir'),
 ]
