c05a7e00a898fb6f3ffe71ab2dcc7f59a774e93b
nautilusdata <rayarza@nautilus.cl>
2026-02-14 23:57:34 -0300

    tienda/productos.views.py ARMANDO DETALLE DE PRODUCTO

--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,12 @@
+from django.http import Http404
 from django.shortcuts import render
+from home.models import *
 
-# Create your views here.
+def productos_detalle(request, id, slug):
+    try: #para chequear que lo que vienepor la URL es válido
+        datos=Producto.objects.filter(pk=id).filter(slug=slug).filter(estado_id=1).get()
+    except Producto.DoesNotExist:
+        raise Http404
+    relacionados=Producto.objects.filter(estado_id=1, producto_categoria=datos.producto_categoria_id).filter(estado_id=1).all()
+    fotos=ProductoFotos.objects.filter(producto_id=id).all()
+    return render(request, 'productos/detalle.html', {'datos':datos, 'relacionados': relacionados, 'fotos':fotos})
