86a5b5552ca9befbe478daee0866afef8223a127
nautilusdata <rayarza@nautilus.cl>
2026-02-15 02:45:04 -0300

    Claude aconseja este cambio en tienda/prductos/views.py

--- productos/views.py
+++ productos/views.py
@@ -3,10 +3,17 @@ from django.shortcuts import render
 from home.models import *
 
 def productos_detalle(request, id, slug):
-    try: #para chequear que lo que vienepor la URL es válido
-        datos=Producto.objects.filter(pk=id).filter(slug=slug).filter(estado_id=1).get()
+    try:
+        datos = Producto.objects.filter(pk=id).filter(slug=slug).filter(estado_id=1).get()
     except Producto.DoesNotExist:
         raise Http404
-    relacionados=Producto.objects.filter(estado_id=1, producto_categoria=datos.producto_categoria_id).filter(estado_id=1).all()
-    fotos=ProductoFotos.objects.filter(producto_id=id).all()
-    return render(request, 'productos/detalle.html', {'datos':datos, 'relacionados': relacionados, 'fotos':fotos})
+    
+    relacionados = Producto.objects.filter(estado_id=1, producto_categoria=datos.producto_categoria_id).filter(estado_id=1).all()
+    fotos = ProductoFotos.objects.filter(producto_id=id).all()
+    
+    return render(request, 'productos/detalle.html', {
+        'datos': datos,
+        'relacionados': relacionados,
+        'fotos': fotos,
+        'stock_range': range(1, datos.stock + 1)  # <-- esto
+    })
