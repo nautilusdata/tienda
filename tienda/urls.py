5c18ed41fdfdd2aa531ba7a91757dfde58fb8c72
nautilusdata <rayarza@nautilus.cl>
2026-02-15 15:02:47 -0300

    ACCESO urls.py se descomenta para empezar a trabajar en Login, registro y envío de  emailde rerificación.

--- tienda/urls.py
+++ tienda/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 urlpatterns = [
     path('core/backend/', admin.site.urls),
     path('', include('home.urls'), name="home_urls"),
-    #path('acceso/', include('acceso.urls'), name="acceso_urls"),
+    path('acceso/', include('acceso.urls'), name="acceso_urls"),
     path('productos/', include('productos.urls'), name="productos_urls"),
     #path('carro/', include('carro.urls'), name="carro_urls"),
 ]
