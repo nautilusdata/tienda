f5207d175f49e7d50cdd3e7c4e76ea5872afcc67
nautilusdata <rayarza@nautilus.cl>
2026-02-15 15:31:48 -0300

    urls.py añadido y archivo views,py de app acceso

--- acceso/urls.py
+++ acceso/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *                    #llamado a la vistas con las que vamos a trabajar
+
 urlspatterns = [
 path('login', acceso_login, name='acceso_login'),  #permite crear form para que user se pueda loguear
 path('registri', acceso_registro, name='acceso_registro'),
--- acceso/views.py
+++ acceso/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect, HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User
+from home.models import *
+from .forms import *
+from django.contrib.auth import authenticate, login, logout
+from datetime import datetime, date, timedelta
+from utilidades import utilidades
+import time
+from django.conf import settings
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+def acceso_login(request):
+    	pass
+
+def acceso_registro(request):
+    	pass
+
+def acceso_salir(request):
+	pass
