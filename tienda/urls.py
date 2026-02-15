5f193deb659406dbc402e51b6cb17657b6a546f0
nautilusdata <rayarza@nautilus.cl>
2026-02-14 23:58:40 -0300

    se descomenta productos de urls principal (previamente escrita en epera)

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('core/backend/', admin.site.urls),
     path('', include('home.urls'), name="home_urls"),
     #path('acceso/', include('acceso.urls'), name="acceso_urls"),
-    #path('productos/', include('productos.urls'), name="productos_urls"),
+    path('productos/', include('productos.urls'), name="productos_urls"),
     #path('carro/', include('carro.urls'), name="carro_urls"),
 ]
