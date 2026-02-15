8fb8142923810987a22fb4e0c0db0a64df02338f
nautilusdata <rayarza@nautilus.cl>
2026-02-15 15:08:08 -0300

    tienda/acceso/urls.py  URLS de  la aplicación , definición de paths

---
+++ acceso/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *                    #llamado a la vistas con las que vamos a trabajar
+urlspatterns = [
+path('login', acceso_login, name='acceso_login'),
+path('registri', acceso_registro, name='acceso_registro'),
+path('salir', acceso_salir, name='acceso_salir'),
+]
