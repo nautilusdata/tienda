2dfb5e1e7ea2115feaa3c7f32e0a2dbe801fe095
nautilusdata <rayarza@nautilus.cl>
2026-02-15 22:46:42 -0300

    creación de tienda/acceso/forms.py - partimos con dos formularios: login y registro.

--- acceso/forms.py
+++ acceso/forms.py
@@ -1,1 +1,17 @@
-from django import forms # solo para poner algo y node lineas amarillas en el editor de codigo, no se va a usar nada de esto por ahora, pero es necesario para que no de error al importar este archivo en las vistas
+from django.db import models
+from django import forms
+from django.forms import ModelForm, PasswordInput #Formulario de paso
+
+
+class Formulario_Login(forms.Form):
+    correo = forms.EmailField(required=True, widget=forms.TextInput(
+        attrs={'class':'form-control', 'placeholder':'E-Mail','autocomplete':'off'}))
+    password = forms.CharField(widget=PasswordInput(attrs={'class':'form-control', 'placeholder':'Contraseña', 'autocomplete':'off'}))
+
+
+class Formulario_Registro(forms.Form):
+    nombre = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nombre', 'autocomplete':'off'}))
+    apellido = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Apellido', 'autocomplete':'off'}))
+    correo = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'E-Mail', 'autocomplete':'off'}))
+    password = forms.CharField(widget=PasswordInput(attrs={'class':'form-control', 'placeholder':'Contraseña', 'autocomplete':'off'}))
+    password2 = forms.CharField(widget=PasswordInput(attrs={'class':'form-control', 'placeholder':'Repetir Contraseña', 'autocomplete':'off'}))
