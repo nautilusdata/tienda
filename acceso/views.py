0b3bb3ac5ec273e3a78c125d61cb25ab3aeb60e5
nautilusdata <rayarza@nautilus.cl>
2026-02-16 02:17:33 -0300

    flash.html NO ESTABA CREADO no aparecia mensaje al registrar usuario nuevo. Ademas views.py modificacin de claude por el mimso tema del mensaje una vez creado el usuario.

--- acceso/views.py
+++ acceso/views.py
@@ -77,7 +77,8 @@ def acceso_registro(request):
                 utilidades.sendMail(html, 'Tienda', request.POST['correo'])
                 mensaje = f"Registro exitoso. Por favor verifique su cuenta a través del enlace que le hemos enviado a su correo electrónico {request.POST['correo']} para activar la cuenta."
                 messages.add_message(request, messages.SUCCESS, mensaje)
-                return HttpResponseRedirect('/acceso/login')
+                form = Formulario_Registro()  # ← formulario limpio
+                return render(request, 'acceso/registro.html', {'form': form})
     return render(request, 'acceso/registro.html', {'form': form})
 
 
