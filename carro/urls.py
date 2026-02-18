72bca5b4fb8d2b3a2533bc9655b6cdd7a036130c
nautilusdata <rayarza@nautilus.cl>
2026-02-18 16:13:18 -0300

    comillas de libreoffice corregidas en carro/urls.py

--- carro/urls.py
+++ carro/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', carro_inicio, name="carro_inicio"),
     path('crear', carro_crear, name="carro_crear"),   							# +creando registros en el carrito
     path('vaciar', carro_vaciar, name="carro_vaciar"),
-    path('quitar-de-carro/<int:id>', carro_quitar, name=”carro_quitar”),
+    path('quitar-de-carro/<int:id>', carro_quitar, name="carro_quitar"),
 ]
