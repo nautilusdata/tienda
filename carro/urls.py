2e15a0f88bad175d498a216b2e47d7dd95340396
nautilusdata <rayarza@nautilus.cl>
2026-02-19 03:08:29 -0300

    faltaba bootstrap.min.css y jquery-3.6.4.min.js dentro de la carpeta tienda/assets/js y /css

--- carro/urls.py
+++ carro/urls.py
@@ -3,7 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', carro_inicio, name="carro_inicio"),
-    path('crear', carro_crear, name="carro_crear"),   							# +creando registros en el carrito
+    path('crear', carro_crear, name="carro_crear"),   							# crear un registro en el carrito
     path('vaciar', carro_vaciar, name="carro_vaciar"),
     path('quitar-de-carro/<int:id>', carro_quitar, name="carro_quitar"),
+    path('modificar-cantidad-carro/<int:id>/<int:cantidad>', carro_modificar_cantidad, name="carro_modificar_cantidad"),
 ]
